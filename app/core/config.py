239e536a2e5cc1731160ceeb0b5a088bc132272f
DilanRamirez <dilanrrr@gmail.com>
2025-05-28 17:49:31 -0500

    added: fixed

--- app/core/config.py
+++ app/core/config.py
@@ -2,6 +2,7 @@
 from dotenv import load_dotenv
 import os
 import boto3
+import sys
 
 load_dotenv()  # Load variables from .env
 
@@ -11,20 +12,23 @@ class Settings:
     DEBUG: bool = os.getenv("DEBUG", "false").lower() == "true"
     GEMINI_API_KEY: str = os.getenv("GEMINI_API_KEY", "")
     ADMIN_KEY: str = os.getenv("ADMIN_KEY", "")
-    AWS_ACCESS_KEY_ID: str = os.environ["AWS_ACCESS_KEY_ID"]
-    AWS_SECRET_KEY: str = os.environ["AWS_SECRET_KEY"]
-    AWS_REGION: str = os.environ["AWS_REGION"]
-    S3_BUCKET: str = os.environ["S3_BUCKET"]
+    AWS_ACCESS_KEY_ID: str = os.getenv("AWS_ACCESS_KEY_ID", "")
+    AWS_SECRET_KEY: str = os.getenv("AWS_SECRET_KEY", "")
+    AWS_REGION: str = os.getenv("AWS_REGION", "")
+    S3_BUCKET: str = os.getenv("S3_BUCKET", "")
 
 
 settings = Settings()
 
 
-s3 = boto3.client(
-    "s3",
-    aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
-    aws_secret_access_key=settings.AWS_SECRET_KEY,
-    region_name=settings.AWS_REGION,
-)
-
-BUCKET = settings.S3_BUCKET
+if "pytest" not in sys.modules:
+    s3 = boto3.client(
+        "s3",
+        aws_access_key_id=settings.AWS_ACCESS_KEY_ID,
+        aws_secret_access_key=settings.AWS_SECRET_KEY,
+        region_name=settings.AWS_REGION,
+    )
+    BUCKET = settings.S3_BUCKET
+else:
+    s3 = None
+    BUCKET = None
