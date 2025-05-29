70e56c53c3e9b5af8fd0e09cfd8e9ff7718c3e9a
DilanRamirez <dilanrrr@gmail.com>
2025-05-29 11:41:14 -0500

    added

--- tests/test_llm_answer_service.py
+++ tests/test_llm_answer_service.py
@@ -3,22 +3,22 @@ from fastapi.testclient import TestClient
 from app.main import app
 
 
-# def test_llm_answer_valid_input():
-#     with TestClient(app) as client:
-#         response = client.post(
-#             "/api/v1/llm/answer",
-#             json={
-#                 "question": "¿Qué significa la fe?",
-#                 "top_k": 3,
-#                 "lang": "es",
-#                 "mode": "explain",
-#             },
-#         )
-#         assert response.status_code == 200
-#         data = response.json()
-#         assert "answer" in data
-#         assert "context_used" in data
-#         assert data["context_used"] > 0
+def test_llm_answer_valid_input():
+    with TestClient(app) as client:
+        response = client.post(
+            "/api/v1/llm/answer",
+            json={
+                "question": "¿Qué significa la fe?",
+                "top_k": 3,
+                "lang": "es",
+                "mode": "explain",
+            },
+        )
+        assert response.status_code == 200
+        data = response.json()
+        assert "answer" in data
+        assert "context_used" in data
+        assert data["context_used"] > 0
 
 
 def test_llm_answer_empty_question():
@@ -54,15 +54,15 @@ def test_llm_answer_invalid_lang():
         assert response.status_code == 422  # lang must be 'en' or 'es'
 
 
-# def test_llm_answer_no_relevant_context():
-#     with TestClient(app) as client:
-#         response = client.post(
-#             "/api/v1/llm/answer",
-#             json={
-#                 "question": "¿Cuál es el número atómico del Uranio?",
-#                 "top_k": 3,
-#                 "lang": "es",
-#             },
-#         )
-#         # 404 if no context; 200 if fallback applies
-#         assert response.status_code in [404, 200]
+def test_llm_answer_no_relevant_context():
+    with TestClient(app) as client:
+        response = client.post(
+            "/api/v1/llm/answer",
+            json={
+                "question": "¿Cuál es el número atómico del Uranio?",
+                "top_k": 3,
+                "lang": "es",
+            },
+        )
+        # 404 if no context; 200 if fallback applies
+        assert response.status_code in [404, 200]
